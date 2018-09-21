c47354177eae2020aafa49221d663d46c6f77d2a
Igor Lutsenko <lutsenkoiv@snlab.ru>
2018-09-21 15:37:45 +0300

    fabric v2 update

--- fabfile.py
+++ fabfile.py
@@ -1,48 +1,101 @@
 # coding=utf-8
 import os
 
-from fabric.api import env
-from fabric.context_managers import cd
-from fabric.decorators import roles
-from fabric.operations import run, local, put
+from fabric import task
+from fabric import Connection
+from patchwork.transfers import rsync
+from invoke.context import Context
 
-env.roledefs['master'] = ['{{ project_name }}@xxx.xxx.xxx.xxx']
-
-
-def _production_env():
-    # Speedup connection setup to server.
-    env.disable_known_hosts = True
-
-    env.key_filename = [os.path.join(os.environ['HOME'], '.ssh', 'id_rsa')]
-    env.project_root = '~/app/'
+key_filename = os.path.join(os.environ['HOME'], '.ssh', 'id_rsa')
+host = 'xxx.xxx.xxx.xxx'
+user = '{{ project_name }}'
+port = 2020
+project_root = '~/app'
 
 
-@roles('master')
-def build(only=False):
+def update_config(connect):
+    '''
+    Заполняем параметры подключения
+    '''
+    # Если в параметрах запуска не задан хост, то тут тип Context
+    if isinstance(connect, Context):
+        connect = Connection(host=host)  # Инициализируем Connection
+    conf = {
+        "user": user,
+        "port": port,
+        "connect_kwargs": {"key_filename": key_filename},
+    }
+    for k, v in conf.items():
+        if hasattr(connect, k):
+            setattr(connect, k, v)
+    return connect
+
+
+@task
+def build(connect_obj, only=False):
+    '''
+    Локальная сборка проекта
+    '''
     if not only or only == 'app':
-        local('docker build -f docker/production/Dockerfile -t {{ project_name }}/projects:{{ project_name }}-app .')
-        local('docker push {{ project_name }}/projects:{{ project_name }}-app')
+        with update_config(connect_obj) as c:
+            c.local('docker build -f docker/production/Dockerfile \
+                -t {{ project_name }}/projects:{{ project_name }}-app .')
+            c.local('docker login')
+            c.local('docker push {{ project_name }}/projects:{{ project_name }}-app')
+
+@task
+def deploy(connect_obj):
+    '''
+    Резвёртывание проекта на сервере
+    '''
+    with update_config(connect_obj) as c:
+        with c.cd(project_root):
+            # run('git pull origin master')
+            c.put('docker/production/docker-compose.yml', 'app/')
+
+            c.run('docker-compose pull')
+            c.run('docker-compose up -d')
 
 
-@roles('master')
-def deploy():
-    _production_env()
-
-    with cd(env.project_root):
-        put('docker/production/docker-compose.yml', '~/app/')
-
-        run('docker-compose pull')
-        run('docker-compose up -d')
+@task
+def build_deploy(connect_obj, only=False):
+    '''
+    Локальная сборка и развёртывание на сервере
+    '''
+    build(connect_obj, only)
+    deploy(connect_obj)
 
 
-@roles('master')
-def build_deploy(only=False):
-    build(only)
-    deploy()
+@task
+def clear_cache(connect_obj):
+    '''
+    Очистка кэша Django на сервере
+    '''
+    with update_config(connect_obj) as c:
+        with c.cd(project_root):
+            c.run('docker-compose exec --rm manage clear_cache')
 
-@roles('master')
-def clear_cache():
-    _production_env()
 
-    with cd(env.project_root):
-        run('docker-compose exec --rm manage clear_cache')
+@task
+def restart(connect_obj):
+    '''
+    Перезапуск докер на сервере
+    '''
+    with update_config(connect_obj) as c:
+        with c.cd(project_root):
+            c.run('docker-compose restart')
+
+
+@task
+def clear_containers(connect_obj):
+    '''
+    Удалить неиспользуемые контейнеры на сервере
+    '''
+    with update_config(connect_obj) as c:
+        with c.cd(project_root):
+            c.run('docker system prune -a')
+
+
+if __name__ == '__main__':
+    server = Connection(host=host)
+    build_deploy(server)
